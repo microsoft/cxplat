0dda3960d1b79aede1543f03a5efb96fc50167cd
Yi Huang <huanyi@microsoft.com>
2024-07-17 17:10:48 -0700

    more

--- inc/cxplatvector.h
+++ inc/cxplatvector.h
@@ -7,12 +7,12 @@ template <typename T>
 class CxPlatVector : public Rtl::KArray<T>
 {
     public:
-    void
+    bool
     push_back(
         _In_ T value
         )
     {
-        CXPLAT_FRE_ASSERT(append(value));
+        return append(value);
     }
 
     const T* data() const { return &(*this)[0]; }
@@ -26,7 +26,7 @@ class CxPlatVector : public Rtl::KArray<T>
         _In_ const const_iterator &end
         )
     {
-        CXPLAT_FRE_ASSERT(insertAt(dest, start, end));
+        return insertAt(dest, start, end);
     }
 };
 #else
