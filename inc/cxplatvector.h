7c48d4c3c25bc8dd83d7effd5d18834fc2f48738
ami-GS <1991.daiki@gmail.com>
2024-09-03 05:32:28 -0700

    Refactoring CxPlatVector

--- inc/cxplatvector.h
+++ inc/cxplatvector.h
@@ -4,33 +4,8 @@
 #include "karray.h"
 
 template <typename T>
-class CxPlatVector : public Rtl::KArray<T>
-{
-    public:
-    using Rtl::KArray<T>::KArray;
+using CxPlatVector = Rtl::KArray<T>;
 
-    bool
-    push_back(
-        _In_ T value
-        )
-    {
-        return append(value);
-    }
-
-    const T* data() const { return &(*this)[0]; }
-
-    size_t size() const { return count(); }
-
-    void
-    insert(
-        _In_ const iterator &dest,
-        _In_ const const_iterator &start,
-        _In_ const const_iterator &end
-        )
-    {
-        return insertAt(dest, start, end);
-    }
-};
 #else
 
 #include <vector>
